07b7be4971a6faebb058eb6108981cbfe7b62c05
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-11-08 22:15:23 +0530

    Style: Footer set to bottom no matter the size of the content of the page

--- app/page.js
+++ app/page.js
@@ -1,13 +1,16 @@
+import styles from "./page.module.css";
 import Footer from "@/components/footer";
 import HomeContent from "@/components/homeContent";
 import Navbar from "@/components/navbar";
 
 export default function LandingPage() {
   return (
-    <div className="wrapper">
-      <Navbar/>
-      <HomeContent/>
-      <Footer/>
+    <div className={styles.wrapper}>
+      <Navbar />
+      <main className={styles.content}>
+        <HomeContent />
+      </main>
+      <Footer />
     </div>
   );
 }
