6895a3fde2f4475758dc361112dad8c1eeec2a6c
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-11-10 18:33:58 +0530

    Perf: job page separated as a component

--- app/event/[eventData]/page.js
+++ app/event/[eventData]/page.js
@@ -1,5 +1,6 @@
 import styles from "./page.module.css";
 import Footer from "@/components/footer";
+import JobDetailsPage from "@/components/jobDetails";
 import Navbar from "@/components/navbar";
 import { supabase } from "@/lib/supabaseClient";
 
@@ -20,67 +21,7 @@ export default async function EventPage({ params }) {
     <div className="wrapper">
       <Navbar />
       <main className="content">
-        <div className={styles.details}>
-          <header className={styles.header}>
-            <img
-              src={data[0].poster}
-              alt="Event Poster"
-              className={styles.poster}
-            />
-            <div className={styles.info}>
-              <h1 className={styles.heading}>{data[0].heading}</h1>
-              <p className={styles.dateTime}>
-                Start: {data[0].start_date} | End: {data[0].end_date}
-              </p>
-              <p className={styles.location}>Location: {data[0].location}</p>
-            </div>
-          </header>
-          <hr />
-          <section className={styles.description}>
-            <h2>About the Job</h2>
-            <p>{data[0].about}</p>
-          </section>
-          <hr />
-
-          <section className={styles.detailsSection}>
-            <div className={styles.item}>
-              <h3>Position</h3>
-              <p>{data[0].position}</p>
-            </div>
-            <div className={styles.item}>
-              <h3>Responsibilities</h3>
-              <ul>
-                {data &&
-                  data[0].responsibilities.map((element, index) => (
-                    <li key={element.id}>{element}</li>
-                  ))}
-              </ul>
-            </div>
-            <div className={styles.item}>
-              <h3>Requirements</h3>
-              <ul>
-                {data &&
-                  data[0].requirements.map((element, index) => (
-                    <li key={element.id}>{element}</li>
-                  ))}
-              </ul>
-            </div>
-            <div className={styles.item}>
-              <h3>Renumeration</h3>
-              <p>{data[0].salary}</p>
-            </div>
-          </section>
-          <hr />
-          <div className={styles.map}>
-            <iframe
-              width="100%"
-              height="100%"
-              loading="lazy"
-              allowFullScreen
-              src={`https://www.google.com/maps/embed/v1/place?key=${process.env.GOOGLE_MAPS_API}&q=${data[0].location}`}
-            ></iframe>
-          </div>
-        </div>
+        <JobDetailsPage data={data}/>
       </main>
       <Footer />
     </div>
