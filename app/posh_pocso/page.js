4773729c9cfc98feae2bc58c5eb8a5b59f27afac
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-12-27 21:14:29 +0530

    Refactor: exchanged about page with posh_pocso page

--- app/posh_pocso/page.js
+++ app/posh_pocso/page.js
@@ -1,87 +1,101 @@
+"use client";
 import styles from "./page.module.css";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
-import Image from "next/image";
+import React from "react";
+import Link from "next/link";
 
-export default function PoshPocsoPage() {
+const AboutPage = () => {
   return (
     <div className="wrapper">
       <Navbar />
-      <main
-        className="content"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <div className={styles.headerContainer}>
-          <div className={styles.info}>
-            <h1>Safe Workspaces, Empowered Lives</h1>
-            <h3>
-              Empowering safe, respectful workplaces with expert POSH and POSCO
-              guidance
-            </h3>
+      <main className="content">
+        <section className={styles.container}>
+          <h1 className={styles.title}>
+            Explore Juris Opportunity&apos;s Legal Expertise
+          </h1>
+          <p className={styles.description}>
+            At Juris Opportunity, we are dedicated to providing exceptional
+            legal services tailored to your unique needs. Discover our
+            commitment to justice and excellence.
+          </p>
+          <div className={styles.buttons}>
+            <Link href={"/"}>
+              <button className={styles.primaryButton}>Get Started</button>
+            </Link>
           </div>
-          <div className={styles.image}>
-            <div className={styles.bgCircle}>
-              <Image src={"/lawyer.png"} height={500} width={500} alt="" />
+          <img src="/library.jpg" alt="" className={styles.library} />
+        </section>
+        <div className={styles.container1}>
+          <div className={styles.feature}>
+            <div className={styles.iconWrapper}>
+              <span className={styles.icon}>🔴</span>
             </div>
+            <h3 className={styles.title1}>Experienced Legal Professionals</h3>
+            <p className={styles.description1}>
+              Our team of seasoned lawyers ensures you receive top-notch legal
+              advice.
+            </p>
+          </div>
+          <div className={styles.feature}>
+            <div className={styles.iconWrapper}>
+              <span className={styles.icon}>🌸</span>
+            </div>
+            <h3 className={styles.title1}>Personalized Legal Services</h3>
+            <p className={styles.description1}>
+              Tailored legal strategies to meet your individual requirements.
+            </p>
+          </div>
+          <div className={styles.feature}>
+            <div className={styles.iconWrapper}>
+              <span className={styles.icon}>📊</span>
+            </div>
+            <h3 className={styles.title1}>Comprehensive Legal Solutions</h3>
+            <p className={styles.description1}>
+              A wide range of services to address all legal needs.
+            </p>
           </div>
         </div>
-        <div className={styles.featuresContainer}>
-          <h1>Your Partner in Legal Safety and Compliance</h1>
-          <section className={styles.features}>
-            <div className={styles.feature}>
-              <Image src={"/track_record.jpg"} height={1000} width={1000} alt="" />
-              <h4>Comprehensive Training Programs</h4>
-              <p>
-                Equip your workforce with essential knowledge and skills to
-                prevent harassment and ensure a safe workplace.
-              </p>
-              <div className={styles.shade}></div>
-            </div>
-            <div className={styles.feature}>
-              <Image src={"/library.jpg"} height={1000} width={1000} alt="" />
-              <h4>Expert Legal Consultation</h4>
-              <p>
-                Offering expert guidance to protect
-                your organization and its employees.
-              </p>
-              <div className={styles.shade}></div>
-            </div>
-            <div className={styles.feature}>
-              <Image src={"/library.jpg"} height={1000} width={1000} alt="" />
-              <h4>Confidential Reporting Mechanisms</h4>
-              <p>
-                Providing secure, confidential channels for employees to report
-                harassment without fear of retaliation.
-              </p>
-              <div className={styles.shade}></div>
-            </div>
-            <div className={styles.feature}>
-              <Image src={"/library.jpg"} height={1000} width={1000} alt="" />
-              <h4>Tailored Compliance Audits</h4>
-              <p>
-                Regular assessments to ensure your organization remains
-                compliant with POSH and POSCO.
-              </p>
-              <div className={styles.shade}></div>
-            </div>
-            <div className={styles.feature}>
-              <Image src={"/library.jpg"} height={1000} width={1000} alt="" />
-              <h4>Employee Awareness Campaigns</h4>
-              <p>
-                Raising awareness and promoting a culture of respect and safety
-                through ongoing initiatives.
-              </p>
-              <div className={styles.shade}></div>
-            </div>
-          </section>
+        <div className={styles.features}>
+          <img src="/client_centered.jpg" alt="" />
+          <div className={styles.info}>
+            <h1>Client-Centered Focus</h1>
+            <p>
+              The Juris Opportunity website emphasizes prioritizing the
+              client&apos;s interests in every legal case it handles. This is
+              reflected in the content and design, which showcase a strong
+              commitment to understanding client needs and tailoring legal
+              solutions accordingly.
+            </p>
+          </div>
+        </div>
+        <div className={styles.features}>
+          <div className={styles.info}>
+            <h1>Transparent Communication</h1>
+            <p>
+              The website highlights a process where clients are kept informed
+              throughout their legal journey. This feature assures users of open
+              communication, fostering trust and confidence.
+            </p>
+          </div>
+          <img src="/communication.jpg" alt="" />
+        </div>
+        <div className={styles.features}>
+          <img src="/track_record.jpg" alt="" />
+          <div className={styles.info}>
+            <h1>Proven Track Record</h1>
+            <p>
+              The Juris Opportunity website highlights a process where clients
+              are kept informed throughout their legal journey. This feature
+              assures users of open communication, fostering trust and
+              confidence.
+            </p>
+          </div>
         </div>
       </main>
       <Footer />
     </div>
   );
-}
+};
+
+export default AboutPage;
