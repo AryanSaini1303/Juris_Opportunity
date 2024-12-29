83377685f57cfe46a9038e2a4a3046350f040147
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-12-29 20:59:20 +0530

    Feat: About us page completed with real data

--- app/about/page.js
+++ app/about/page.js
@@ -9,6 +9,26 @@ export default function AboutPage() {
   const [hover, setHover] = useState(0);
   const [members, setMembers] = useState();
   const [loading, setLoading] = useState(true);
+  const [hover1, setHover1] = useState(0);
+
+  const [info, setInfo] = useState();
+  const [infoLoading, infoLetLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchAboutUs = async () => {
+      try {
+        const response = await fetch(`/api/getAboutUs`);
+        const data = await response.json();
+        console.log(data);
+        setInfo(data);
+        infoLetLoading(false);
+      } catch (error) {
+        console.error("Error fetching advisory board:", error);
+        infoLetLoading(false);
+      }
+    };
+    fetchAboutUs();
+  }, []);
 
   useEffect(() => {
     const fetchMembers = async () => {
@@ -31,11 +51,8 @@ export default function AboutPage() {
       <Navbar />
       <main className="content" style={{ padding: "1rem 5rem" }}>
         <section className={styles.info}>
-          <h1>Safe Workspaces, Empowered lives</h1>
-          <p>
-            Empowering safe, respectful workplaces with expert POSH and POSCO
-            guidance
-          </p>
+          <h1>Driving Excellence in Legal Solutions, Empowering Growth</h1>
+          <p>{info ? info[0].description : null}</p>
         </section>
         <section className={styles.members}>
           {loading ? (
@@ -72,6 +89,43 @@ export default function AboutPage() {
             <p>No members found!</p>
           )}
         </section>
+        {members && (
+          <section className={styles.cardsSection}>
+            {!infoLoading &&
+              info &&
+              info.map((element, index) => {
+                if (index == 0) return;
+                return (
+                  <div className={styles.cardContainer} key={element.id}>
+                    <div
+                      className={styles.infoCard}
+                      onMouseEnter={() => {
+                        setHover1(index + 1);
+                      }}
+                      onMouseLeave={() => {
+                        setHover1(0);
+                      }}
+                      style={
+                        hover1 == index + 1
+                          ? { transform: "rotateX(180deg)" }
+                          : null
+                      }
+                    >
+                      {hover1 != index + 1 ? (
+                        <h3>{element.heading}</h3>
+                      ) : (
+                        <ul style={element.description.length==1?{listStyleType:"none", padding:0}:null}>
+                          {element.description.map((element1, index)=>(
+                          <li key={index}>{element1}</li>
+                        ))}
+                        </ul>
+                      )}
+                    </div>
+                  </div>
+                );
+              })}
+          </section>
+        )}
       </main>
       <Footer />
     </div>
