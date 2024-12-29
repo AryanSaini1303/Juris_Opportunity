0c11d6709ba1cc164584055e16743b76a2699ed2
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-12-29 17:25:31 +0530

    Feat: advisory page complete with real data

--- app/advisory/page.js
+++ app/advisory/page.js
@@ -1,229 +1,78 @@
 "use client";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
-import Image from "next/image";
 import styles from "./page.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import LoaderComponent from "@/components/loader";
 
 export default function AdvisoryPage() {
   const [click, setClick] = useState(0);
+  const [members, setMembers] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchAdvisory = async () => {
+      try {
+        const response = await fetch(`/api/getAdvisory`);
+        const data = await response.json();
+        console.log(data);
+        setMembers(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching advisory board:", error);
+        setLoading(false);
+      }
+    };
+    fetchAdvisory();
+  }, []);
+
   return (
     <div className="wrapper">
       <Navbar />
       <main className="content">
         <header className={styles.header}>Our Advisory Board</header>
-        <section className={styles.cardsSection}>
-          <div
-            className={styles.card}
-            tabIndex={0}
-            style={
-              click == 1
-                ? { transform:"translateY(-5px)", boxShadow:"0 8px 12px rgba(0, 0, 0, 0.2)" }
-                :null
-            }
-            onClick={() => {
-              setClick(1);
-            }}
-            onBlur={() => {
-              setClick(0);
-            }}
-          >
-            <Image
-              src={"/support.jpg"}
-              width={100}
-              height={100}
-              alt=""
-              style={click == 1 ? { display:"none" } : null}
-            />
-            {click != 1 ? (
-              <div className={styles.info}>
-                <h1>Abhishek Yadav</h1>
-                <h4>UI/UX Designer</h4>
-              </div>
-            ) : (
-              <p>
-                Passionate UI/UX designer creating intuitive, user-friendly
-                interfaces, blending creativity and functionality to enhance
-                user experiences across digital platforms.
-              </p>
-            )}
-          </div>
-          <div
-            className={styles.card}
-            style={
-              click == 2
-                ? { transform:"translateY(-5px)", boxShadow:"0 8px 12px rgba(0, 0, 0, 0.2)" }
-                :null
-            }
-            tabIndex={0}
-            onClick={() => {
-              setClick(2);
-            }}
-            onBlur={() => {
-              setClick(0);
-            }}
-          >
-            <Image
-              src={"/support.jpg"}
-              width={100}
-              height={100}
-              alt=""
-              style={click == 2 ? { display:"none" } : null}
-            />
-            {click != 2 ? (
-              <div className={styles.info}>
-                <h1>Abhishek Yadav</h1>
-                <h4>UI/UX Designer</h4>
-              </div>
-            ) : (
-              <p>
-                Passionate UI/UX designer creating intuitive, user-friendly
-                interfaces, blending creativity and functionality to enhance
-                user experiences across digital platforms.
-              </p>
-            )}
-          </div>
-          <div
-            className={styles.card}
-            style={
-              click == 3
-                ? { transform:"translateY(-5px)", boxShadow:"0 8px 12px rgba(0, 0, 0, 0.2)" }
-                :null
-            }
-            tabIndex={0}
-            onClick={() => {
-              setClick(3);
-            }}
-            onBlur={() => {
-              setClick(0);
-            }}
-          >
-            <Image
-              src={"/support.jpg"}
-              width={100}
-              height={100}
-              alt=""
-              style={click == 3 ? { display:"none" } : null}
-            />
-            {click != 3 ? (
-              <div className={styles.info}>
-                <h1>Abhishek Yadav</h1>
-                <h4>UI/UX Designer</h4>
-              </div>
-            ) : (
-              <p>
-                Passionate UI/UX designer creating intuitive, user-friendly
-                interfaces, blending creativity and functionality to enhance
-                user experiences across digital platforms.
-              </p>
-            )}
-          </div>
-          <div
-            className={styles.card}
-            style={
-              click == 4
-                ? { transform:"translateY(-5px)", boxShadow:"0 8px 12px rgba(0, 0, 0, 0.2)" }
-                :null
-            }
-            tabIndex={0}
-            onClick={() => {
-              setClick(4);
-            }}
-            onBlur={() => {
-              setClick(0);
-            }}
-          >
-            <Image
-              src={"/support.jpg"}
-              width={100}
-              height={100}
-              alt=""
-              style={click == 4 ? { display:"none" } : null}
-            />
-            {click != 4 ? (
-              <div className={styles.info}>
-                <h1>Abhishek Yadav</h1>
-                <h4>UI/UX Designer</h4>
-              </div>
-            ) : (
-              <p>
-                Passionate UI/UX designer creating intuitive, user-friendly
-                interfaces, blending creativity and functionality to enhance
-                user experiences across digital platforms.
-              </p>
-            )}
-          </div>
-          <div
-            className={styles.card}
-            style={
-              click == 5
-                ? { transform:"translateY(-5px)", boxShadow:"0 8px 12px rgba(0, 0, 0, 0.2)" }
-                :null
-            }
-            tabIndex={0}
-            onClick={() => {
-              setClick(5);
-            }}
-            onBlur={() => {
-              setClick(0);
-            }}
-          >
-            <Image
-              src={"/support.jpg"}
-              width={100}
-              height={100}
-              alt=""
-              style={click == 5 ? { display:"none" } : null}
-            />
-            {click != 5 ? (
-              <div className={styles.info}>
-                <h1>Abhishek Yadav</h1>
-                <h4>UI/UX Designer</h4>
-              </div>
-            ) : (
-              <p>
-                Passionate UI/UX designer creating intuitive, user-friendly
-                interfaces, blending creativity and functionality to enhance
-                user experiences across digital platforms.
-              </p>
-            )}
-          </div>
-          <div
-            className={styles.card}
-            style={
-              click == 6
-                ? { transform:"translateY(-5px)", boxShadow:"0 8px 12px rgba(0, 0, 0, 0.2)" }
-                :null
-            }
-            tabIndex={0}
-            onClick={() => {
-              setClick(6);
-            }}
-            onBlur={() => {
-              setClick(0);
-            }}
-          >
-            <Image
-              src={"/support.jpg"}
-              width={100}
-              height={100}
-              alt=""
-              style={click == 6 ? { display:"none" } : null}
-            />
-            {click != 6 ? (
-              <div className={styles.info}>
-                <h1>Abhishek Yadav</h1>
-                <h4>UI/UX Designer</h4>
-              </div>
-            ) : (
-              <p>
-                Passionate UI/UX designer creating intuitive, user-friendly
-                interfaces, blending creativity and functionality to enhance
-                user experiences across digital platforms.
-              </p>
-            )}
-          </div>
-        </section>
+        {!loading ? (
+          <section className={styles.cardsSection}>
+            {members &&
+              members.map((element, index) => (
+                <div
+                  className={styles.card}
+                  tabIndex={0}
+                  key={element.id}
+                  style={
+                    click == `${index + 1}`
+                      ? {
+                          transform: "translateY(-5px)",
+                          boxShadow: "0 8px 12px rgba(0, 0, 0, 0.2)",
+                        }
+                      : null
+                  }
+                  onClick={() => {
+                    click == index + 1 ? setClick(0) : setClick(index + 1);
+                  }}
+                  onBlur={() => {
+                    setClick(0);
+                  }}
+                >
+                  <img
+                    src={element.image}
+                    alt=""
+                    style={click == `${index + 1}` ? { display: "none" } : null}
+                  />
+                  {click != `${index + 1}` ? (
+                    <div className={styles.info}>
+                      <h1>{element.name}</h1>
+                      <h4>{element.role}</h4>
+                    </div>
+                  ) : (
+                    <p>{element.description}</p>
+                  )}
+                </div>
+              ))}
+          </section>
+        ) : (
+          <LoaderComponent/>
+        )}
       </main>
       <Footer />
     </div>
