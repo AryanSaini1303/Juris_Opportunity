7695fa8318ef6792aca946e24665cc13fee98d01
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-12-29 18:05:03 +0530

    Refactor: added real data to the about us page from database

--- app/about/page.js
+++ app/about/page.js
@@ -1,12 +1,31 @@
 "use client";
+import LoaderComponent from "@/components/loader";
 import styles from "./page.module.css";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
-import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function AboutPage() {
   const [hover, setHover] = useState(0);
+  const [members, setMembers] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMembers = async () => {
+      try {
+        const response = await fetch(`/api/getMembers`);
+        const data = await response.json();
+        console.log(data);
+        setMembers(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching advisory board:", error);
+        setLoading(false);
+      }
+    };
+    fetchMembers();
+  }, []);
+
   return (
     <div className="wrapper">
       <Navbar />
@@ -19,121 +38,39 @@ export default function AboutPage() {
           </p>
         </section>
         <section className={styles.members}>
-          <div
-            onMouseEnter={() => {
-              setHover(1);
-            }}
-            onMouseLeave={() => {
-              setHover(0);
-            }}
-          >
-            <Image
-              src="/member1.jpg"
-              alt=""
-              className={styles.member1}
-              width={220}
-              height={350}
-            />
-            {hover==1 && (
-              <section className={styles.memberInfo}>
-                <h2>Sasha Grey</h2>
-                <p>Head of Designs</p>
-              </section>
-            )}
-            <section className={styles.shade}></section>
-          </div>
-          <div
-            onMouseEnter={() => {
-              setHover(2);
-            }}
-            onMouseLeave={() => {
-              setHover(0);
-            }}
-          >
-            <Image
-              src="/member2.jpg"
-              alt=""
-              className={styles.member2}
-              width={220}
-              height={350}
-            />
-            {hover==2 && (
-              <section className={styles.memberInfo}>
-                <h2>Mathew Brians</h2>
-                <p>Junior Developer</p>
-              </section>
-            )}
-            <section className={styles.shade}></section>
-          </div>
-          <div
-            onMouseEnter={() => {
-              setHover(3);
-            }}
-            onMouseLeave={() => {
-              setHover(0);
-            }}
-          >
-            <Image
-              src="/member3.jpg"
-              alt=""
-              className={styles.member3}
-              width={220}
-              height={350}
-            />
-            {hover==3 && (
-              <section className={styles.memberInfo}>
-                <h2>Xiaomi chang</h2>
-                <p>Database Administrator</p>
-              </section>
-            )}
-            <section className={styles.shade}></section>
-          </div>
-          <div
-            onMouseEnter={() => {
-              setHover(4);
-            }}
-            onMouseLeave={() => {
-              setHover(0);
-            }}
-          >
-            <Image
-              src="/member4.jpg"
-              alt=""
-              className={styles.member4}
-              width={220}
-              height={350}
-            />
-            {hover==4 && (
-              <section className={styles.memberInfo}>
-                <h2>Scarlett Watson</h2>
-                <p>Sales </p>
-              </section>
-            )}
-            <section className={styles.shade}></section>
-          </div>
-          <div
-            onMouseEnter={() => {
-              setHover(5);
-            }}
-            onMouseLeave={() => {
-              setHover(0);
-            }}
-          >
-            <Image
-              src="/member5.jpg"
-              alt=""
-              className={styles.member5}
-              width={220}
-              height={350}
-            />
-            {hover==5 && (
-              <section className={styles.memberInfo}>
-                <h2>Howard Potts</h2>
-                <p>Lead Developer</p>
-              </section>
-            )}
-            <section className={styles.shade}></section>
-          </div>
+          {loading ? (
+            <LoaderComponent />
+          ) : members ? (
+            members.map((element, index) => (
+              <div
+                onMouseEnter={() => {
+                  setHover(index + 1);
+                }}
+                onMouseLeave={() => {
+                  setHover(0);
+                }}
+                key={element.id}
+                className={styles.card}
+              >
+                <img
+                  src={element.image}
+                  alt=""
+                  className={`styles.member${index + 1}`}
+                  width={220}
+                  height={350}
+                />
+                {hover == index + 1 && (
+                  <section className={styles.memberInfo}>
+                    <h2>{element.name}</h2>
+                    <p>{element.role}</p>
+                  </section>
+                )}
+                <section className={styles.shade}></section>
+              </div>
+            ))
+          ) : (
+            <p>No members found!</p>
+          )}
         </section>
       </main>
       <Footer />
---
+++ pages/api/getMembers.js
@@ -0,0 +1,18 @@
+import { supabase } from "@/lib/supabaseClient";
+
+export default async function handler(req, res) {
+  try {
+    const { data, error } = await supabase
+      .from("members")
+      .select("*")
+      .order("id", { ascending: true });
+
+    if (error) {
+      throw error;
+    }
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
